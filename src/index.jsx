6fc5f2a5e6bfe751746843fa4e204549b1985c1e
Riccardo Moncada <rmondev@gmail.com>
2025-03-13 14:57:58 -0400

    added project, author and date info

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,6 @@
-
+// Package Name: Scrimba - Learn React Course - Meme Generator
+// Created by Riccardo Moncada (rmondev)
+// Date: March 13, 2025
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
